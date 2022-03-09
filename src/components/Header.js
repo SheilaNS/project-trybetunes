57f81c86b6214172eda06297267edd7228d5b17c
SheilaNS <shei.nsantos@gmail.com>
2022-03-09 11:49:13 -0300

    Requisito 05: Crie o formulário para pesquisar artistas - ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,11 +27,11 @@ class Header extends Component {
     return (
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
+        <h2 data-testid="header-user-name">{name}</h2>
+        {loading && <Loading />}
         <Link to="/search" data-testid="link-to-search">Procurar</Link>
         <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
         <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
-        <h2 data-testid="header-user-name">{name}</h2>
-        {loading && <Loading />}
       </header>
     );
   }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,60 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+      name: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const TAM = 2;
+    const valid = target.value.length < TAM;
+    if (valid) {
+      this.setState(
+        {
+          disabled: true,
+          name: target.value,
+        },
+      );
+    } else {
+      this.setState(
+        {
+          disabled: false,
+          name: target.value,
+        },
+      );
+    }
+  }
+
   render() {
+    const { disabled, name } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <form>
+            <label htmlFor="search">
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                id="search"
+                name="search"
+                onChange={ this.handleChange }
+                value={ name }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ disabled }
+            >
+              Perquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
